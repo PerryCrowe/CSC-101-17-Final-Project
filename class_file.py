2f928dc0ef9ceafe7d0ee8e25d84bcf523046b64
Perry Crowe <pecrowe@calpoly.edu>
2026-03-08 16:52:22 -0700

    Commented out the first "import" line

--- class_file.py
+++ class_file.py
@@ -1,4 +1,5 @@
-import
+#import
+
 class Statistics:
     # Initializing a new Statistics object
     # input: Cal Poly's water usage data as a dictionary
@@ -10,7 +11,7 @@ class Statistics:
     def __init__(self,
                  year: int,
                  water:dict[str,float],
-                 energy:dict[str, float],
+                 energy:dict[str, float | dict[str,float]],
                  transportation:dict[str,float],
                  waste:float):
         self.year = year
