67675013f9a759f43594476248d09a5ffce24388
ggazzano007 <ggazzano@calpoly.edu>
2026-03-08 11:02:30 -0700

    function file

---
+++ function_file.py
@@ -0,0 +1,17 @@
+
+from class_file import Statistics
+
+def filter_by_year(data: [Statistics], year: int) -> [Statistics]:
+    result = []
+    for statistics in data:
+        if statistics.year == year:
+            result.append(statistics)
+
+
+
+
+#total reneawable source of electricity
+#if idx 1 is true
+
+#comparision from 1 year to another year and see increase of specific dataclasses
+
