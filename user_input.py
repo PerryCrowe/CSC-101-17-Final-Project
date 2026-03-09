b3a7dbaacf4eef65b239fa02e062cbe021c53519
Perry Crowe <pecrowe@calpoly.edu>
2026-03-09 10:21:33 -0700

    Adjusted "gather_data" function

--- user_input.py
+++ user_input.py
@@ -2,7 +2,12 @@ from dataset import *
 import sys
 
 def gather_data ():
-    result = []
-    while True:
-        user_input = input("Enter Year:",
-                           "Enter Statistic from list:
+    user_input = []
+    print("Would you like to compare two reports or look at one report?")
+    ans = input("Please input 'One' or 'Two'")
+    if ans.lower() == 'one':
+        user_input.append(input("Enter Desired Year:"))
+        user_input.append(input("Enter Statistic from list:"))
+    return user_input
+
+print(gather_data())
