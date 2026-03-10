60caaeca807592fbc6ca6eb17131b5332b30a658
ggazzano007 <ggazzano@calpoly.edu>
2026-03-09 20:16:22 -0700

    finished the first part!

--- user_input.py
+++ user_input.py
@@ -1,62 +1,59 @@
-import dataset
 from dataset import reports_by_year
-import sys
 
 def gather_data ():
-    user_input = []
     print("Would you like to compare two reports or look at one report?")
-    ans = input("Please input 'One' or 'Two'")
+    ans = input("Please input 'One' or 'Two': ")
 
-    year = int(input("Enter Desired Year:"))
+    year = int(input("Enter Desired Year: "))
     report = reports_by_year[year]
-    category = input("Enter category (energy, transportation, water, waste):")
-    answer = input("")
+    category = input("Enter category (energy, transportation, water, waste): ")
+
 
     if category.lower () == "energy":
         print("BTUs per Square Foot, Sources of Electricity, or % of Campus Vehicle Fleet Using Alternative Fuel?")
+        answer = input("")
 
-        if answer == "BTUs per Square Foot":
+        if answer.lower() == "btus per square foot":
             print(report.energy["BTUs per Square Foot"])
-        elif answer == "% of Campus Vehicle Fleet Using Alternative Fuel":
+        elif answer.lower() == "% of campus vehicle fleet using alternative fuel":
             print(report.energy["% of Campus Vehicle Fleet Using Alternative Fuel"])
-        elif answer == "Sources of Electricity":
+        elif answer.lower() == "sources of electricity":
             print ("Renewable, Large Hydro, Nuclear, Natural Gas, Coal, or Unspecified?")
             sub = input("")
-            sources = report.energy["Sources of Electricity"]
+            sources = report.energy["Sources of Electricity (print exactly):"]
             if sub in sources:
                 print(sources[sub])
 
 
-    if category.lower == "Transportation":
+    if category.lower() == "transportation":
         print ("Parking Permits per Student, % of Student Population Living on Campus, or CP SLO Transit Riders per Year?")
+        answer = input("")
 
-        if answer == "Parking Permits per Student":
-            print( report.transportation["Parking Permits per Student"])
-        if answer == "% of Student Population Living on Campus":
+        if answer.lower() == "parking permits per student":
+            print(report.transportation["Parking Permits per Student"])
+        elif answer.lower() == "% of student population living on campus":
             print(report.transportation["% of Student Population Living on Campus"])
-        elif answer == "CP SLO Transit Riders per Year":
+        elif answer.lower() == "cp slo transit riders per year":
             print(report.transportation["CP SLO Transit Riders per Year"])
 
-    if answer == "Water":
+
+    if category.lower() == "water":
         print ("Domestic Water Use, Total Delivered Water, or Total Indoor Water Use?")
-        if answer == "Domestic Water Use":
+        answer = input("")
+
+        if answer.lower() == "domestic water use":
             print( report.water["Domestic Water Use"])
-        if answer == "Total Delivered Water":
+        elif answer.lower() == "total delivered water":
             print( report.water["Total Delivered Water"])
-        if answer == "Total Indoor Water":
+        elif answer.lower() == "total indoor water":
             print( report.water["total indoor Water"])
 
-    if answer == "Waste":
-        print( report.waste)
 
-
-
-
-
+    if category.lower() == "waste":
+        print(report.waste)
 
 if __name__ == "__main__":
-    user_input = gather_data()
-    print(user_input)
+    gather_data()
 
 
 
