2a7438abdbc96bb09f3fea6e92cb5cfcf794fbb1
ggazzano007 <ggazzano@calpoly.edu>
2026-03-10 18:23:55 -0700

    done!

--- tests.py
+++ tests.py
@@ -1,24 +1,80 @@
-# import dataset
-# import unittest
-# from function_file import (is_increasing, filter_by_year)
-#
-# class TestCase(unittest.TestCase):
-#
-#     def test_is_increasing(self):
-#         report1 = dataset.Statistics(
-#             year = 2016,
-#             energy = {},
-#             transportation = {},
-#             waste = ,
-#             water = {}
-#         )
-#         report2 = dataset.Statistics(
-#             year = 2010,
-#             energy = {},
-#             transportation = {},
-#             waste = ,
-#             water = {}
-#         )
-#
-#         reports = [report1, report2]
+import dataset
+import unittest
+from function_file import (is_increasing, filter_by_year)
 
+class TestCase(unittest.TestCase):
+
+    def test_is_increasing_energy(self):
+        report1 = dataset.Statistics(
+            year = 2015,
+            energy = {"BTUs per Square Foot": 650000},
+            transportation = {},
+            water = {},
+            waste = 0
+        )
+        report2 = dataset.Statistics(
+            year = 2010,
+            energy = {"BTUs per Square Foot": 620000},
+            transportation = {},
+            water = {},
+            waste = 0
+        )
+        print(is_increasing(report1, report2, "energy","BTUs per Square Foot",None))
+
+
+    def test_is_increasing_transportation(self):
+        report1 = dataset.Statistics(
+            year = 2015,
+            energy = {},
+            transportation = {"CP SLO Transit Riders per Year": 400000},
+            water = {},
+            waste = 0
+        )
+        report2 = dataset.Statistics(
+            year = 2010,
+            energy = {},
+            transportation = {"CP SLO Transit Riders per Year": 820000},
+            waste = 0,
+            water = {}
+        )
+        print(is_increasing(report1, report2, "transportation","CP SLO Transit Riders per Year",None))
+
+
+    def test_is_increasing_water(self):
+        report1 = dataset.Statistics(
+            year = 2015,
+            energy = {},
+            transportation = {},
+            water = {"Total Delivered Water": 1180},
+            waste = 0
+        )
+        report2 = dataset.Statistics(
+            year = 2010,
+            energy = {},
+            transportation = {},
+            water = {"Total Delivered Water": 3000},
+            waste = 0
+        )
+        print(is_increasing(report1, report2, "water", "Total Delivered Water", None))
+
+
+    def test_is_increasing_waste(self):
+        report1 = dataset.Statistics(
+            year = 2015,
+            energy = {},
+            transportation = {},
+            water = {},
+            waste = 93.0
+        )
+        report2 = dataset.Statistics(
+            year = 2010,
+            energy = {},
+            transportation = {},
+            water = {},
+            waste = 52.3
+        )
+        print(is_increasing(report1, report2, "waste", "Waste", None))
+
+
+if __name__ == '__main__':
+    unittest.main()
